444583a2997d916f0e3cd9ec3faee243a32323ca
Zlatomrezov_Aleksey <zlatomrezovaleksey@gmail.com>
2025-02-10 16:13:54 +0300

    added componentDidVount in User

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,10 +3,9 @@ import s from"./Users.module.css";
 import axios from "axios";
 import userPhoto from "../../assets/images/user.png"
 
-class Users extends React.Component {
+class Users extends React.Component {  
 
-    constructor (props) {
-        super(props);        
+    componentDidMount(){
         axios.get("https://social-network.samuraijs.com/api/1.0/users")
             .then(response => {
                 this.props.setUsers(response.data.items);
