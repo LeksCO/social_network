e1eb8377dd95e0fbc3c32bfad539beebe9486419
Zlatomrezov_Aleksey <zlatomrezovaleksey@gmail.com>
2025-02-11 20:38:42 +0300

    add paggination in Users

--- src/redux/usersReaduser.js
+++ src/redux/usersReaduser.js
@@ -1,13 +1,17 @@
 const FOLLOW = "FOLLOW";
 const UNFOLLOW = "UNFOLLOW"
 const SET_USERS = "SET_USERS"
+const SET_CURRENT_PAGE = "SET_CURRENT_PAGE"
+const SET_TOTAL_USERS_COUNT = "SET_TOTAL_USERS_COUNT"
+
+
 
 let initialState = {
-    users : [/*
-    { id: 1, photoUrl:"https://avatars.mds.yandex.net/i?id=ac5dc23bebc784cc695efd419cb41f11ffc78774-7716469-images-thumbs&n=13", followed: true, fullname: "Aleksey", status: "I'm the BOSS of the boss",location: {city: "Azov", country: "Russia"}},
-    { id: 2, photoUrl:"https://avatars.mds.yandex.net/i?id=455a674be6206399095149572e3544963307bc91-4079110-images-thumbs&n=13",followed: true, fullname: "Anna", status: "I'm the Cool",location: {city: "Azov", country: "Russia"}},
-    { id: 3, photoUrl:"https://avatars.mds.yandex.net/i?id=ec1fe8b6402abb88b8b86979ee8f4ca16a1a6ad15cb7b610-4577377-images-thumbs&n=13",followed: false, fullname: "Aleksandr", status: "I'm the boss",location: {city: "St. Petersburg", country: "Russia"}}
-  */],}
+    users : [ ],
+    pageSize: 5, 
+    totalUsersCount: 0,
+    currentPage: 4,
+};
 
 const usersReaduser = (state = initialState, action) => {
     switch(action.type){ 
@@ -32,7 +36,13 @@ const usersReaduser = (state = initialState, action) => {
                 })
             }
         case SET_USERS: {
-            return {...state, users: [ ...state.users, ...action.users ]}
+            return {...state, users: [ ...action.users ]}
+        }
+        case SET_CURRENT_PAGE: {
+            return {...state, currentPage: action.currentPage}
+        }
+        case SET_TOTAL_USERS_COUNT: {
+            return {...state, totalUsersCount: action.count}
         }
 
             default:
@@ -44,3 +54,5 @@ export default usersReaduser;
 export const followAC = (userId) => ({type: FOLLOW, userId})
 export const unfollowAC = (userId) => ({type: UNFOLLOW, userId})
 export const setUsersAC = (users) => ({type: SET_USERS, users})
+export const setCurrentPageAC = (currentPage) => ({type: SET_CURRENT_PAGE, currentPage})
+export const setTotalUsersCountAC = (totalUsersCount) => ({type: SET_TOTAL_USERS_COUNT, count: totalUsersCount})
