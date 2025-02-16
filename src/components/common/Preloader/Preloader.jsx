d2d558cde208ee6af7b8d83e8d71285de9648c55
Zlatomrezov_Aleksey <zlatomrezovaleksey@gmail.com>
2025-02-16 15:18:03 +0300

    added preloader

---
+++ src/components/common/Preloader/Preloader.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import preloader from "../../../assets/images/loader.svg"
+let Preloader = (props) => {
+    return <div style={{backgroundColor: "green"}}><img src={preloader}/></div>
+}
+
+export default Preloader;
