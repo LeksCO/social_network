91ab84b0f9b805d1db796318f9b7b187c80aacf1
Zlatomrezov_Aleksey <zlatomrezovaleksey@gmail.com>
2025-02-18 23:35:23 +0300

    added post and delete for users

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import s from"./Users.module.css";
 import userPhoto from "../../assets/images/user.png"
 import { NavLink } from "react-router-dom";
+import axios from "axios";
 
 let Users = (props) => {
 
@@ -31,8 +32,37 @@ let Users = (props) => {
             </div>
             <div>
                 { u.followed
-                ? <button onClick = {() => {props.unfollow(u.id)}}>Unfollow</button> 
-                : <button onClick = {() => {props.follow(u.id)}}>Follow</button>}
+                ? <button onClick = {() => {
+
+                    axios.delete(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {
+                        withCredentials: true,
+                        headers: {
+                            "API-KEY":"a2f1a583-49a3-406e-a13d-8ebe31786ad5"
+                        }
+                    })
+                        .then(response => {
+                            if(response.data.resultCode == 0){
+                                props.unfollow(u.id)
+                            }
+                        });
+
+                }}>Unfollow</button> 
+
+                : <button onClick = {() => {
+                        
+                        axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {}, {
+                            withCredentials: true,
+                            headers: {
+                                "API-KEY":"a2f1a583-49a3-406e-a13d-8ebe31786ad5"
+                            }
+                        })
+                            .then(response => {
+                                if(response.data.resultCode == 0){
+                                    props.follow(u.id)
+                                }
+                            });
+                    
+                    }}>Follow</button>}
             </div>
             <div key={u.id}>{u.followed}</div>
         </span>
